539d7f15bf976d2483999cce8280cce597926316
BeneteAndre <andrebenete@hotmail.com>
2016-04-29 09:09:51 -0300

    atualização1

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -37,9 +37,29 @@ namespace Api.Controllers
         }
 
         // GET: api/Product/5
-        public string Get(int id)
+        public IHttpActionResult Get(int id)
         {
-            return "value";
+            var produto1 = new Product();
+            produto1.Id = 1;
+            produto1.Name = "Placa de rede";
+            produto1.Category = "Hardware";
+            produto1.Price = 120m;
+            listaDeProduto.Add(produto1);
+
+            var produto2 = new Product();
+            produto2.Id = 2;
+            produto2.Name = "Placa de som";
+            produto2.Category = "Hardware";
+            produto2.Price = 110m;
+            listaDeProduto.Add(produto2);
+
+            var produto3 = new Product { Id = 3, Name = "Norton Antivirus", Category = "Software", Price = 70m };
+            listaDeProduto.Add(produto3);
+
+
+            var produto = listaDeProduto.Find(x => x.Id == id);
+            return Ok(produto);
+
         }
 
         // POST: api/Product
